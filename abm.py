94ad90e8f867267f95291cd7c9260681dbbc57b4
HaywardPeirce <hpeirce@gmail.com>
2018-12-31 07:30:07 +0000

    HP: clean up code and add comments

--- abm.py
+++ abm.py
@@ -15,14 +15,8 @@ json_url = config['ABM']['JSONURL']
 with open('abm-value-config.json') as f:
     abmValues = json.load(f)
 
-#print(type(influxPort))
-
-#influx_client = InfluxDBClient(influxAddress, influxPort, influxUser, influxPassword, influxDatabase)
-
 def getWeatherData(json_url):
     try:
-        # requestURL = 'http://api.wunderground.com/api/' + weatherapikey + '/conditions/q/' + str(cityid)+ '.json'
-    
         response = requests.get(json_url)
     
         data = json.loads(response.text)
@@ -58,10 +52,10 @@ def formatData(data):
         }
     ]
     
+    # make sure that the nessesary tags are present, otherwise exit
     try:
-        # make sure that the nessesary tags are present, otherwise exit
-        # TODO: should we return null?
         
+        #configure the tags for the entry
         json_data[0]['tags']['name'] = (str(data['observations']['header'][0]['name']))
         json_data[0]['tags']['ID'] = (str(data['observations']['header'][0]['ID']))
         json_data[0]['tags']['main_ID'] = (str(data['observations']['header'][0]['main_ID']))
@@ -82,26 +76,25 @@ def formatData(data):
         print("Unable to collect tag info: {}".format(e))
         return None
     
-    print("sort_order", data['observations']['data'][0]['sort_order'])
-    print(data['observations']['data'][0])
-    
-    print(abmValues)
-    
+    # Loop through each of the fields that have been configured in the `abm-value-config.json` file
     for entry in abmValues:
-        print(entry, " ", abmValues[entry])
+
         if entry in data['observations']['data'][0]:
             
+            # if the entry is null, just keep it as null and move on the next entry (null values will be omitted from being sent on to influx by influx)
             if data['observations']['data'][0][entry] == None:
                 json_data[0]['fields'][entry] = data['observations']['data'][0][entry]
                 continue
             
+            # if the field has been configured to be formatted as a string
             elif abmValues[entry] == "str":
                 json_data[0]['fields'][entry] = str(data['observations']['data'][0][entry])
-                
+            
+            # if the field has been configured to be formatted as a float    
             elif abmValues[entry] == "float":
                 json_data[0]['fields'][entry] = float(data['observations']['data'][0][entry])
     
-    print(json_data)
+    #print(json_data)
 
     return json_data
 
